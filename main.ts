41b5df9cc5db21df3d81340a11f9742405e426cc
carlos676767 <carlitobr730@gmail.com>
2024-08-25 15:40:28 -0700

    estou desenvolvendo este projeto para buscar vagas de empregos para meu uso pessoal.

---
+++ main.ts
@@ -0,0 +1,9 @@
+import cheerio from "cheerio";
+import pupper from "puppeteer";
+import fs from "fs";
+
+class BrowserManager {
+  static abrirNavegador(): void {
+    const brower = pupper.launch({})
+  }
+}
